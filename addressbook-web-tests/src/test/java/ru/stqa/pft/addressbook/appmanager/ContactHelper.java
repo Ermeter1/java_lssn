a79038cac460165da963b68e5b413e7974232847
Ermeter1 <ermeter@mail.ru>
2017-01-27 01:36:45 +0300

    Задание 10 удаление, создание и модификация контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -54,8 +54,8 @@ public class ContactHelper extends HelperBase{
         wd.switchTo().alert().accept();
     }
 
-    public void initContactModification() {
-        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    public void initContactModification(int index) {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[" + (index +2) + "]/td[8]/a/img"));
     }
 
     public void submitContactModification() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,7 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase{
 
@@ -12,10 +17,24 @@ public class ContactModificationTests extends TestBase{
             //при создании контакта указывается группа [none], дабы не делать проверку на наличие указываемой группы
             app.getContactHelper().createContact(new ContactData("Bilbo", "Baggins", "Shire","8(800)8008080","bilbo@mail.ru", "[none]"), true);
         }
-        app.getContactHelper().initContactModification();
-        app.getContactHelper().fillContactForm(new ContactData("B","B","B","B","B", null), false);
+        List<ContactData> before = app.getContactHelper().getContactList();
+        //app.getContactHelper().selectContact(before.size()-1);
+        app.getContactHelper().initContactModification(before.size()-1);
+        ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "B","B","B","B","B", null);
+        app.getContactHelper().fillContactForm(contact, false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size()-1);
+        before.add(contact);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId()) ;
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
+
+
     }
 
 }
