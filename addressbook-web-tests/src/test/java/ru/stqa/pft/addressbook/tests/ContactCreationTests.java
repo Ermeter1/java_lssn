5a53d2e3eb92d54ed28765fac61de57473d4acce
Ermeter1 <ermeter@mail.ru>
2017-01-27 01:00:53 +0300

    задание 10 тест удаления контакта, не закончен

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -11,7 +14,16 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation(){
         app.getNavigationHelper().gotoHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
-        //здесь нужна бы проверка на наличие группы test1, либо указывать группу [none]
-        app.getContactHelper().createContact(new ContactData("Bilbo", "Baggins", "Shire","8(800)8008080","bilbo@mail.ru", "test1"), true);
+        ContactData contact = new ContactData("Imen", "Familiev", "Adres", "8(900)1112233", "Imen@mail.ru", "test1");
+        //ContactData contact = new ContactData("Bilbo", "Baggins", "Shire","8(800)8008080","bilbo@mail.ru", "test1");
+        app.getContactHelper().createContact(contact, true);
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size()+1);
+
+        before.add(contact);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,20 +1,30 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase{
 
     @Test
     public void testContactDeletion(){
-        //app.getNavigationHelper().gotoHomePage();
+        app.getNavigationHelper().gotoHomePage();
         if (! app.getContactHelper().isThereAContact()){
             //при создании контакта указывается группа [none], дабы не делать проверку на наличие указываемой группы
             app.getContactHelper().createContact(new ContactData("Bilbo", "Baggins", "Shire","8(800)8008080","bilbo@mail.ru", "[none]"), true);
         }
-        app.getContactHelper().selectContact();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size() - 1);
         //при выполнении теста почему то не удаляется контакт, (тест проходит успешно, но контакт не удален) если дебажить, то контакт удаляется, не понятно.
         app.getContactHelper().deleteSelectedContact();
+        app.getNavigationHelper().gotoHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size()-1);
+
+        before.remove(before.size()-1);
+        Assert.assertEquals(before, after);
 
     }
 }
